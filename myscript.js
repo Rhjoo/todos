3a0419c6eeafafaccf182d84b187c1ed785ecc10
Rhjoo <ryanhjoo@gmail.com>
2017-12-09 21:14:43 -0800

    finishing V11
    Destroy all for loops
    replace them with forEach in toggleAll and displayTodos functions

--- myscript.js
+++ myscript.js
@@ -19,20 +19,42 @@ var todoList = {
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    //for (var i = 0; i < totalTodos; i++) {
+    //  if (this.todos[i].completed === true) {
+    //    completedTodos++;
+    //  }
+    //}
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+    });
+
+    this.todos.forEach(function(todo) {
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      } else {
+        todo.completed = true;
       }
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
-    }
+    });
+    //if (completedTodos === totalTodos) {
+    //  for (var i = 0; i < totalTodos; i++) {
+    //    this.todos[i].completed = false;
+    //  }
+
+    //if (completedTodos === totalTodos) {
+    //  this.todos.forEach(function(todo) {
+    //    if (todo.completed = false) {
+    //    }
+    //  });
+    //} else {
+    //  this.todos.forEach(function(todo) {
+    //    todo.completed = true;
+    //  });
+      //for (var i = 0; i < totalTodos; i++) {
+      //  this.todos[i].completed = true;
+      //}
+    //}
   }
 };
 
@@ -73,23 +95,38 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
-      //debugger;
+    // for (var i = 0; i < todoList.todos.length; i++) {
+    //   //debugger;
+    //   var todoLi = document.createElement('li');
+    //   var todo = todoList.todos[i];
+    //   var todoTextWithCompletion = '';
+    //
+    //   if (todo.completed === true) {
+    //     todoTextWithCompletion = '(x) ' + todo.todoText;
+    //   } else {
+    //     todoTextWithCompletion = '( ) ' + todo.todoText;
+    //   }
+    //
+    //   todoLi.id = i;
+    //   todoLi.textContent = todoTextWithCompletion;
+    //   todoLi.appendChild(this.createDeleteButton());
+    //   todosUl.appendChild(todoLi);
+    // }
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
-        todoTextWithCompletion = '(x) ' + todo.todoText;
-      } else {
-        todoTextWithCompletion = '( ) ' + todo.todoText;
-      }
+           todoTextWithCompletion = '(x) ' + todo.todoText;
+         } else {
+           todoTextWithCompletion = '( ) ' + todo.todoText;
+         }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this)
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
