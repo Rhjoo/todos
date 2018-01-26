68156b531e7ffca8f1cb73bd57a67c8eaca88c21
Rhjoo <ryanhjoo@gmail.com>
2018-01-25 23:53:29 -0800

    cleaning up comments

--- myscript.js
+++ myscript.js
@@ -19,11 +19,7 @@ var todoList = {
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
-    //for (var i = 0; i < totalTodos; i++) {
-    //  if (this.todos[i].completed === true) {
-    //    completedTodos++;
-    //  }
-    //}
+
     this.todos.forEach(function(todo) {
       if (todo.completed === true) {
         completedTodos++;
@@ -37,24 +33,6 @@ var todoList = {
         todo.completed = true;
       }
     });
-    //if (completedTodos === totalTodos) {
-    //  for (var i = 0; i < totalTodos; i++) {
-    //    this.todos[i].completed = false;
-    //  }
-
-    //if (completedTodos === totalTodos) {
-    //  this.todos.forEach(function(todo) {
-    //    if (todo.completed = false) {
-    //    }
-    //  });
-    //} else {
-    //  this.todos.forEach(function(todo) {
-    //    todo.completed = true;
-    //  });
-      //for (var i = 0; i < totalTodos; i++) {
-      //  this.todos[i].completed = true;
-      //}
-    //}
   }
 };
 
@@ -95,23 +73,7 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    // for (var i = 0; i < todoList.todos.length; i++) {
-    //   //debugger;
-    //   var todoLi = document.createElement('li');
-    //   var todo = todoList.todos[i];
-    //   var todoTextWithCompletion = '';
-    //
-    //   if (todo.completed === true) {
-    //     todoTextWithCompletion = '(x) ' + todo.todoText;
-    //   } else {
-    //     todoTextWithCompletion = '( ) ' + todo.todoText;
-    //   }
-    //
-    //   todoLi.id = i;
-    //   todoLi.textContent = todoTextWithCompletion;
-    //   todoLi.appendChild(this.createDeleteButton());
-    //   todosUl.appendChild(todoLi);
-    // }
+
     todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
       var todoTextWithCompletion = '';
